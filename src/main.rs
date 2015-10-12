7c1f1ebc891d1c5c1627a87e2615a5176d969e5a
Jason Dusek <jason.dusek@gmail.com>
2015-10-11 23:53:09 -0700

    Expand sketch.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 extern crate chrono;
 
 fn main() {
-    let s = Countdown::default();
+    // TODO: argument parsing
+    let countdown = Countdown::default();
     println!("Countdown Initiated!");
+    for mark in countdown {
+        match mark {
+            Minute(t) => {
+                println!("T minus {} minutes and counting...",
+                         /* round time difference to minutes */);
+            },
+            Second(t) => {
+                println!("{}...", /* round time difference to seconds */);
+            },
+            Liftoff(s) => {
+                println!(s);
+            }
+        }
+    }
 }
+
+type UTCTime = chrono::datetime::DateTime<chrono::offset::utc::UTC>;
 
 struct Countdown {
     /// The power word used to end the countdown
     liftoff: String,
     /// When the countdown ends
-    deadline: chrono::datetime::DateTime<chrono::offset::utc::UTC>,
+    deadline: UTCTime,
     /// Final countdown
     countdown: i8
+}
+
+enum Mark {
+    Minute(UTCTime),
+    Second(UTCTime),
+    Liftoff(String)
 }
 
 /// Default is: 3...2...1...Go!
@@ -20,5 +43,12 @@ impl Default for Countdown {
         let t = chrono::offset::utc::UTC::now()
               + chrono::duration::Duration::seconds(3);
         Countdown { liftoff: "Go!".to_string(), deadline: t, countdown: 3 }
+    }
+}
+
+impl Iterator for Countdown {
+    fn next(&mut self) -> Option<Mark> {
+        // thread.sleep()
+        // yield next thing
     }
 }
