8f4e347fd5618fa88bd62d0997f35913409f16d0
Jason Dusek <jason.dusek@gmail.com>
2015-10-21 00:28:06 -0700

    Use goals -- more accurate and uses fewer syscalls

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,13 @@ fn main() {
     println!("Countdown Initiated!");
     for mark in countdown {
         match mark {
-            Mark::Minute(t) => {
+            Minute(t) => {
                 println!("T minus {} minutes and counting...", t);
             },
-            Mark::Second(t) => {
+            Second(t) => {
                 println!("{}...", t);
             },
-            Mark::Liftoff(s) => {
+            Liftoff(s) => {
                 println!("{}", s);
             }
         }
@@ -36,6 +36,7 @@ struct Countdown {
     countdown: u8
 }
 
+#[derive(Debug)]
 enum Mark {
     Minute(u32),
     Second(u32),
@@ -56,30 +57,50 @@ impl Iterator for Countdown {
     type Item = Mark;
 
     fn next(&mut self) -> Option<Mark> {
-        type SecondsAndNanos = libc::types::os::common::posix01::timespec;
+        let (goal, diff) = self.step();
+        nanosleep(&diff);
+        return if diff < Duration::zero() { None } else { Some(goal) };
+    }
+}
 
-        let req = SecondsAndNanos{tv_sec: 1, tv_nsec: 0};
-        let mut rem = SecondsAndNanos{tv_sec: 0, tv_nsec: 0};
+impl Countdown {
+    fn step(&mut self) -> (Mark, Duration) {
+        let diff = self.deadline - UTC::now();
+        let countdown = self.countdown as i64;
+        let (cmp1m, cmpct, cmp1s) = (diff.num_minutes().cmp(&1),
+                                     diff.num_seconds().cmp(&countdown),
+                                     diff.num_seconds().cmp(&1));
+        let (goal, dur) = match (cmp1m, cmpct, cmp1s) {
+            (Greater, Greater, _) | (Equal, Greater, _) =>
+                (Minute(diff.num_minutes() as u32),
+                 diff - Duration::minutes(diff.num_minutes())),
+            (_, Greater, _) =>
+                (Second(self.countdown as u32),
+                 diff - Duration::seconds(countdown)),
+            (_, _, Greater) | (_, _, Equal) =>
+                (Second(diff.num_seconds() as u32),
+                 diff - Duration::seconds(diff.num_seconds())),
+            (_, _, _) => (Liftoff(self.liftoff.clone()), diff)
+        };
+        return (goal, dur);
+    }
+}
 
-        loop {
-            let d = self.deadline - UTC::now();
-            unsafe { libc::funcs::posix88::unistd::nanosleep(&req, &mut rem); }
+/// Call nanosleep to sleep the given duration, handling interruptions.
+fn nanosleep(dur: &Duration) {
+    use libc::types::os::common::posix01::timespec as SecondsAndNanos;
 
-            match (d.num_minutes().cmp(&1),
-                   d.num_seconds().cmp(&(self.countdown as i64)),
-                   d.num_seconds() % 60,
-                   d.num_seconds().cmp(&1),
-                   d.cmp(&Duration::seconds(0))) {
-                (Greater, _, 0, _, _) | (Equal, _, 0, _, _) =>
-                    return Some(Minute(d.num_minutes() as u32)),
-                (_, Equal, _, _, _) | (_, Less, _, Greater, _) |
-                (_, Less, _, Equal, _) =>
-                    return Some(Second(d.num_seconds() as u32)),
-                (_, _, _, _, Equal) | (_, _, _, Less, Greater) =>
-                    return Some(Liftoff(self.liftoff.clone())),
-                (_, _, _, _, Less) => return None,
-                (_, _, _, _, _)    => {}
-            }
-        }
+    let seconds = dur.num_seconds();
+    let nanos = (*dur - Duration::seconds(seconds)).num_nanoseconds()
+                                                   .expect("Catastrophe!");
+
+    let mut req = SecondsAndNanos{tv_sec: seconds, tv_nsec: nanos};
+    let mut rem = SecondsAndNanos{tv_sec: 0, tv_nsec: 0};
+
+    unsafe { libc::funcs::posix88::unistd::nanosleep(&req, &mut rem); }
+    while rem.tv_sec > 0 || rem.tv_nsec > 0 {
+        req.tv_sec = rem.tv_sec;
+        req.tv_nsec = rem.tv_nsec;
+        unsafe { libc::funcs::posix88::unistd::nanosleep(&req, &mut rem); }
     }
 }
